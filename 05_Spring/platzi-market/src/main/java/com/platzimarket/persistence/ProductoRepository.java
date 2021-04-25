4e824abcbd2541f06b6b0a906e506f6b23486972
ivaladez-09 <ivan.valadez.0902@gmail.com>
2021-04-24 19:25:34 -0500

    New examples of an API using Spring Data JPA

--- 05_Spring/platzi-market/src/main/java/com/platzimarket/persistence/ProductoRepository.java
+++ 05_Spring/platzi-market/src/main/java/com/platzimarket/persistence/ProductoRepository.java
@@ -1,23 +1,56 @@
 package com.platzimarket.persistence;
 
+import com.platzimarket.domain.Product;
+import com.platzimarket.domain.repository.ProductRepository;
 import com.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.platzimarket.persistence.entity.Producto;
+import com.platzimarket.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public class ProductoRepository {
+@Repository
+public class ProductoRepository implements ProductRepository {
+    @Autowired  // Dependency injection. It has to be from the spring framework
     private ProductoCrudRepository productoCrudRepository;
 
-    public List<Producto> getAll(){
-        return (List<Producto>) productoCrudRepository.findAll();
+    @Autowired
+    private ProductMapper productMapper;
+
+    @Override
+    public List<Product> getAll(){
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return productMapper.toProducts(productos);
     }
 
-    public List<Producto> getByCategoria(int idCategoria){
-        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId){
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(productMapper.toProducts(productos));
     }
 
-    public Optional<List<Producto>> getEscasos(int cantidad){
-        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity){
+        Optional<List<Producto>> productos = productoCrudRepository.
+                findByCantidadStockLessThanAndEstado(quantity, true);
+        return productos.map(prods -> productMapper.toProducts(prods));
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId){
+        return productoCrudRepository.findById(productId).map(producto -> productMapper.toProduct(producto));
+    }
+
+    @Override
+    public Product save(Product product){
+        Producto producto = productMapper.toProducto(product);
+        return productMapper.toProduct(productoCrudRepository.save(producto));
+    }
+
+    @Override
+    public void delete(int productId){
+        productoCrudRepository.deleteById(productId);
     }
 }
---
+++ 05_Spring/platzi-market/src/main/java/com/platzimarket/web/controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.platzimarket.web.controller;
+
+import com.platzimarket.domain.Product;
+import com.platzimarket.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/all")
+    public List<Product> getAll(){
+        return productService.gelAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId){
+        return productService.getProduct(productId);
+    }
+
+    @GetMapping("/category/{id}")
+    public Optional<List<Product>> getByCategory(@PathVariable("id") int categoryId){
+        return productService.getByCategory(categoryId);
+    }
+
+    public Optional<List<Product>> getScarseProducts(int quantity){
+        return productService.getScarseProducts(quantity);
+    }
+
+    @PostMapping("/save")
+    public Product save(@RequestBody Product product){
+        return productService.save(product);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable("id") int productId){
+        return productService.delete(productId);
+    }
+}
